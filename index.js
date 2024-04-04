881d16b53d147f54bf3c4fe6c44d7d459e446e29
WesleyDDev <wesleydiashs@gmail.com>
2024-04-04 02:12:18 -0300

    NOVA ATUALIZAÇÃO

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
  })
+
+ function abrirModal(){
+    const modal = document.getElementById('janela_modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click',(e) => {
+      if(e.target.id =='fechar' || e.target.id == 'janela_modal'){
+        modal.classList.remove('abrir')
+      }
+    }
+    )}
+
+
+
+    //interaçao_cards
+
+    function sumir_cards(){
+      const sumir = document.getElementById('bloco_cards')
+      }
+      
